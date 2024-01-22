b5bb35de98a8e70e16e7a58fb7caf023218d7cb4
harshjoshi233 <149490127+harshjoshi233@users.noreply.github.com>
2024-01-22 16:30:30 +0530

    make proper frame testcases

---
+++ Test Suites/Recall_Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Recall_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4f634f4f-7903-4fa4-9d60-0a04fd43ce5e</testSuiteGuid>
+   <testCaseLink>
+      <guid>30cd982b-fdc0-4a57-b3e4-39aa612bace1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Senior_Testcase/Recall_login/TC-02_Edit_interest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
